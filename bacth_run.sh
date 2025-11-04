356bf70750a22ad4502eb1ab64ca091c81cfc50d
scut2024hjt <202420143336@mail.scut.edu.cn>
2025-11-04 16:55:49 +0800

    批量运行脚本

--- bacth_run.sh
+++ bacth_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+python3 run.py --lb fecmp --pfc 1 --irn 0 --simul_time 0.1 --netload 50 --topo leaf_spine_128_100G_OS2 --cdf transformer_moe_cdf 2>&1 > /dev/null&
 python3 run.py --lb drill --pfc 1 --irn 0 --simul_time 0.1 --netload 50 --topo leaf_spine_128_100G_OS2 --cdf transformer_moe_cdf 2>&1 > /dev/null&
 python3 run.py --lb letflow --pfc 1 --irn 0 --simul_time 0.1 --netload 50 --topo leaf_spine_128_100G_OS2 --cdf transformer_moe_cdf 2>&1 > /dev/null&
 python3 run.py --lb conga --pfc 1 --irn 0 --simul_time 0.1 --netload 50 --topo leaf_spine_128_100G_OS2 --cdf transformer_moe_cdf 2>&1 > /dev/null&
